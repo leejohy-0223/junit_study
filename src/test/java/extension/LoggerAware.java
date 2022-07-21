c593c83b77e0cfd0401d0fbeb2ff277f5a9cf12a
leejohy-0223 <leejohy@naver.com>
2022-07-21 20:40:15 +0900

    TestInstancePostProcessor를 구현한 LoggingExtension 테스트

---
+++ src/test/java/extension/LoggerAware.java
@@ -0,0 +1,8 @@
+package extension;
+
+import org.apache.logging.log4j.Logger;
+
+public interface LoggerAware {
+
+    void setLogger(Logger logger);
+}
---
+++ src/test/java/extension/LoggerTest.java
@@ -0,0 +1,21 @@
+package extension;
+
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(LoggingExtension.class)
+class LoggerTest implements LoggerAware {
+
+    private Logger logger;
+
+    @Override
+    public void setLogger(Logger logger) {
+        this.logger = logger;
+    }
+
+    @Test
+    void test_di() {
+        logger.error("di is success!");
+    }
+}
